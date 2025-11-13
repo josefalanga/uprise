1f39f6c76d72d71c9b756185251a1a8a7a51c786
Jose Falanga <jose.falanga@gmail.com>
2025-11-13 16:57:23 -0300

    build 404

--- lib/functions/subshell/build_page.sh
+++ lib/functions/subshell/build_page.sh
@@ -2,34 +2,34 @@
 #############
 # DESCRIPTION
 #############
-# Builds a page from a source .md file and outputs the built version to 'index.html' in the same directory
+# Builds a page from a source .md file and outputs the built version to "$output_name" in the same directory
 #
 #############
 # Usage: 
-# build_page source.md
+# build_page source.md [output.html]
 
 build_page() (
 
 # Switch to page directory
-page=$(basename $1)
-cd $(dirname $1)
+page=$(basename "$1")
+output_name=${2:-"index.html"}
+cd $(dirname "$1")
 
 get_page_metadata $page
 
 if [[ $is_toc == "true" ]]; then
         build_toc "$page"
 elif [[ $process_markdown == "false" ]]; then
-        build_header index.html
-        cat $page | sed -e '1,/END ARISE/d' | cat >> index.html
-        build_footer index.html
+        build_header "$output_name"
+        cat $page | sed -e '1,/END ARISE/d' | cat >> "$output_name"
+        build_footer "$output_name"
 else
-        build_header index.html
+        build_header "$output_name"
         # Grab everything after the Arise metadata block, run it through pandoc to convert to html, and append to our file in progress
-        cat $page | sed -e '1,/END ARISE/d' | pandoc -f markdown -t html >> index.html
-        build_footer index.html
+        cat $page | sed -e '1,/END ARISE/d' | pandoc -f markdown -t html >> "$output_name"
+        build_footer "$output_name"
 fi
 
 # Inline Evaluations - DISABLED, WIP, ENABLE AT YOUR OWN PERIL
-# evaluate_inline index.html
-
+# evaluate_inline "$output_name"
 )
